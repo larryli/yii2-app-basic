08384f831bd0883f6ccb76082c799c532b01958b
Larry Li <larryli@qq.com>
2019-12-29 17:28:33 +0800

    revert addon var for build & deploy

--- build.sh
+++ build.sh
@@ -17,11 +17,32 @@ fi
 
 echo "Building Dockerfile-based application..."
 
+build_secret_args=''
+if [[ -n "$AUTO_DEVOPS_BUILD_IMAGE_FORWARDED_CI_VARIABLES" ]]; then
+  build_secret_file_path=/tmp/auto-devops-build-secrets
+  "$(dirname "$0")"/export-build-secrets > "$build_secret_file_path"
+  build_secret_args="--secret id=auto-devops-build-secrets,src=$build_secret_file_path"
+
+  echo 'Activating Docker BuildKit to forward CI variables with --secret'
+  export DOCKER_BUILDKIT=1
+fi
+
 docker image pull "$CI_APPLICATION_REPOSITORY:composer" || true
 
 # Build the composer stage:
 docker build --target composer \
   --cache-from "$CI_APPLICATION_REPOSITORY:composer" \
+  $build_secret_args \
+  --build-arg BUILDPACK_URL="$BUILDPACK_URL" \
+  --build-arg HTTP_PROXY="$HTTP_PROXY" \
+  --build-arg http_proxy="$http_proxy" \
+  --build-arg HTTPS_PROXY="$HTTPS_PROXY" \
+  --build-arg https_proxy="$https_proxy" \
+  --build-arg FTP_PROXY="$FTP_PROXY" \
+  --build-arg ftp_proxy="$ftp_proxy" \
+  --build-arg NO_PROXY="$NO_PROXY" \
+  --build-arg no_proxy="$no_proxy" \
+  $AUTO_DEVOPS_BUILD_IMAGE_EXTRA_ARGS \
   --tag "$CI_APPLICATION_REPOSITORY:composer" .
 
 docker push "$CI_APPLICATION_REPOSITORY:composer"
@@ -32,6 +53,17 @@ docker image pull "$CI_APPLICATION_REPOSITORY:builder" || true
 docker build --target builder \
   --cache-from "$CI_APPLICATION_REPOSITORY:composer" \
   --cache-from "$CI_APPLICATION_REPOSITORY:builder" \
+  $build_secret_args \
+  --build-arg BUILDPACK_URL="$BUILDPACK_URL" \
+  --build-arg HTTP_PROXY="$HTTP_PROXY" \
+  --build-arg http_proxy="$http_proxy" \
+  --build-arg HTTPS_PROXY="$HTTPS_PROXY" \
+  --build-arg https_proxy="$https_proxy" \
+  --build-arg FTP_PROXY="$FTP_PROXY" \
+  --build-arg ftp_proxy="$ftp_proxy" \
+  --build-arg NO_PROXY="$NO_PROXY" \
+  --build-arg no_proxy="$no_proxy" \
+  $AUTO_DEVOPS_BUILD_IMAGE_EXTRA_ARGS \
   --tag "$CI_APPLICATION_REPOSITORY:builder" .
 
 docker push "$CI_APPLICATION_REPOSITORY:builder"
@@ -48,6 +80,17 @@ docker build \
   --cache-from "$CI_APPLICATION_REPOSITORY:builder" \
   --cache-from "$CI_APPLICATION_REPOSITORY:$CI_COMMIT_BEFORE_SHA" \
   --cache-from "$CI_APPLICATION_REPOSITORY:latest" \
+  $build_secret_args \
+  --build-arg BUILDPACK_URL="$BUILDPACK_URL" \
+  --build-arg HTTP_PROXY="$HTTP_PROXY" \
+  --build-arg http_proxy="$http_proxy" \
+  --build-arg HTTPS_PROXY="$HTTPS_PROXY" \
+  --build-arg https_proxy="$https_proxy" \
+  --build-arg FTP_PROXY="$FTP_PROXY" \
+  --build-arg ftp_proxy="$ftp_proxy" \
+  --build-arg NO_PROXY="$NO_PROXY" \
+  --build-arg no_proxy="$no_proxy" \
+  $AUTO_DEVOPS_BUILD_IMAGE_EXTRA_ARGS \
   --tag "$CI_APPLICATION_REPOSITORY:$CI_APPLICATION_TAG" \
   --tag "$CI_APPLICATION_REPOSITORY:latest" .
 
